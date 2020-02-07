46c4caaaa1cec1b617ff252fb3f2590c75e13208
Xiohexia <xiohexia@outlook.com>
2020-02-07 11:34:56 -0600

    Add project files.

---
+++ DiscordBot/BotCommands.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace DiscordBot
+{
+    public class BotCommands
+    {
+        [Command("hi"), Description("Hello There...")]
+        public async Task Hi(CommandContext ctx)
+        {
+            await ctx.RespondAsync($"Hello There... {ctx.User.Mention}");
+        }
+
+        [Command("eggplant"), Description("Eggplant Emoji")]
+        public async Task Email(CommandContext ctx)
+        {
+            DiscordEmoji emoji = DiscordEmoji.FromName(ctx.Client, ":eggplant:");
+            await ctx.RespondAsync($"{emoji}");
+        }
+    }
+}
---
+++ DiscordBot/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static DiscordClient discord;
+        static CommandsNextModule commands;
+        static void Main(string[] args)
+        {
+            //https://discordapp.com/oauth2/authorize?client_id=675157130742988830&scope=bot&permissions=383040
+
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "Njc1MTU3MTMwNzQyOTg4ODMw.XjzILg.gmAA66iALCV2p5_prPQSLh88HjI",
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = "."
+            });
+            commands.RegisterCommands<BotCommands>();
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
